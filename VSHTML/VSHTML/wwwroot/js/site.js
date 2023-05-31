ee2b7af7f4dbacdd7aaa43e33f5791a06c77815e
pirschous <110510027+pirschous@users.noreply.github.com>
2023-05-31 12:31:29 -0700

    Add files via upload

---
+++ VSHTML/VSHTML/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+// Addded an alert to show wha tsong is playing
+
+function myFunction() {
+    alert("The song now playing is, True Love by SOJA.")
+}
